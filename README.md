00f2528db477f99b48304670d5a029e1809683fe
Will <wb815@ic.ac.uk>
2023-02-10 14:59:49 +0000

    Added support for running as a service

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Scheduler Tools
 
 Basic tools for automating some PBS work. In progress, and potentially unsafe.
+
+## Usage
+
+Recommended: Run the provided tools as services (with the `-s` flag), in a designated environment.
+
+```
+conda create -n schedtools python=3.8
+conda activate schedtools
+pip install -e .
+```
+
+This should allow the service to access the correct Python installation upon repeated reboots, and ensures that
+the environment is stable.
--- src/schedtools/service.py
+++ src/schedtools/service.py
@@ -6,6 +6,7 @@ def make_service(name, command, environment):
     if env_str:
         env_str = f"\n{env_str}"
     python = sys.executable
+    service_id = {name.lower().replace(' ', '-')}.service
     service_config = f"""[Unit]
 Description={name} Service
 
@@ -18,9 +19,9 @@ User=root
 WantedBy=multi-user.target
 """
 
-    filename = f"/etc/systemd/system/{name.lower().replace(' ', '-')}.service"
+    filename = f"/etc/systemd/system/{service_id}"
     subprocess.call(["sudo", "bash", "-c", "echo '" + service_config + "' > " + filename])
 
     subprocess.call(["sudo", "systemctl", "daemon-reload"])
-    subprocess.call(["sudo", "systemctl", "enable", "example.service"])
-    subprocess.call(["sudo", "systemctl", "start", "example.service"])
+    subprocess.call(["sudo", "systemctl", "enable", service_id])
+    subprocess.call(["sudo", "systemctl", "start", service_id])
