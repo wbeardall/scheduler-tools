935d3ddd01436da44f43eb3b9ba70caa7b8f0d1d
Will <wb815@ic.ac.uk>
2023-02-22 10:50:31 +0000

    Added prevention of repeat re-queueing in rerun

--- src/schedtools/jobs.py
+++ src/schedtools/jobs.py
@@ -9,6 +9,7 @@ from schedtools.pbs_dataclasses import PBSJob
 from schedtools.shell_handler import ShellHandler
 
 PRIORITY_RERUN_FILE = "$HOME/.priority-rerun"
+SUCCESS_RERUN_FILE = "$HOME/.success-rerun"
 
 def get_job_percentage(handler: ShellHandler):
     """Get percentage completion of current running / queued jobs.
@@ -52,6 +53,19 @@ def get_rerun_from_file(handler: ShellHandler):
         return []
     raw = json.loads("\n".join(result.stdout))
     return [PBSJob({"id":k, "jobscript_path":v}) for k,v in raw.items()]
+
+def get_success_from_file(handler: ShellHandler):
+    f"""Get successful rerun list from file
+
+    Expects any successfully rerun jobs to be stored in `{SUCCESS_RERUN_FILE}` (json-formatted)
+    
+    Args:
+        handler: `ShellHandler` instance to use to query cluster
+    """
+    result = handler.execute(f"cat {SUCCESS_RERUN_FILE}")
+    if result.returncode:
+        return []
+    return json.loads("\n".join(result.stdout))
 
 def delete_queued_duplicates(
     handler: Union[ShellHandler, str], 
@@ -106,20 +120,40 @@ def rerun_jobs(handler: Union[ShellHandler, str], threshold: Union[int, float]=9
     jobs = manager.get_jobs()
     
     priority_rerun = get_rerun_from_file(handler)
+    to_skip = get_success_from_file(handler)
     priority_ids = [el["id"] for el in priority_rerun]
     new_rerun = [job for job in jobs if job.percent_completion >= threshold and not job.id in priority_ids]
     to_rerun = [el for chunk in [priority_rerun, new_rerun] for el in chunk]
-    for_future = []
+    succeeded = []
+    for skip in to_skip:
+        for i, job in enumerate(to_rerun):
+            # Add to succeeded to ensure that the job isn't requeued next call.
+            # If the job wasn't going to be rerun, we don't need to save the job for
+            # skipping in future.
+            if job.id == skip:
+                succeeded.append(to_rerun.pop(i))
+                break
+
+
+    failed = []
     if len(to_rerun):
         for job in to_rerun:
             try: 
                 manager.rerun_job(job)
+                succeeded.append(job)
             except JobSubmissionError:
-                for_future.append(job)
+                failed.append(job)
                 
-    if len(for_future):
+
+    if len(succeeded):
         # Log any jobs that haven't been able to be requeued
-        future_json = json.dumps({job.id:job.jobscript_path for job in for_future})
-        result = handler.execute("echo '" + future_json + f"\n' > {PRIORITY_RERUN_FILE}")
+        success_json = json.dumps([job.id for job in succeeded])
+        result = handler.execute("echo '" + success_json + f"\n' > {SUCCESS_RERUN_FILE}")
+        if result.returncode:
+            logger.info(f"Saving successful jobs failed with status {result.returncode} ({result.stderr[0].strip()})")
+    if len(failed):
+        # Log any jobs that haven't been able to be requeued
+        failed_json = json.dumps({job.id:job.jobscript_path for job in failed})
+        result = handler.execute("echo '" + failed_json + f"\n' > {PRIORITY_RERUN_FILE}")
         if result.returncode:
             logger.info(f"Saving priority jobs failed with status {result.returncode} ({result.stderr[0].strip()})")
