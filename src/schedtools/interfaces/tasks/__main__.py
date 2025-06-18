8d1cf621f97cc69cc5bf42c029dc43bc797c1699
wbeardall <will.beardall@gmail.com>
2025-06-18 09:39:02 +0100

    Added support for compressing jobs from db

--- src/schedtools/interfaces/tasks/__main__.py
+++ src/schedtools/interfaces/tasks/__main__.py
@@ -1,5 +1,6 @@
 from .task import tasks  # type: ignore
 from .zip_experiments import *  # noqa: F403
+from .zip_tracked_completed import *  # noqa: F403
 
 if __name__ == "__main__":
     tasks()
--- src/schedtools/interfaces/tasks/zip_experiments.py
+++ src/schedtools/interfaces/tasks/zip_experiments.py
@@ -2,50 +2,11 @@ import logging
 import multiprocessing as mp
 import os
 import re
-from typing import List, Union
 
+from schedtools.compression import zip_experiment
 from schedtools.interfaces.tasks.task import queue_task
 
 logger = logging.getLogger(__name__)
-
-
-def build_zip(
-    zip_file: str, path: str, exclude_patterns: Union[str, List[str], None] = None
-) -> None:
-    """Add files to a zip archive.
-
-    Args:
-        zip_file: Path to the zip file
-        path: Path to the directory to zip
-    """
-    import zipfile
-
-    if exclude_patterns is None:
-        exclude_patterns = []
-    elif isinstance(exclude_patterns, str):
-        exclude_patterns = [exclude_patterns]
-    exclude_compiled = [re.compile(pattern) for pattern in exclude_patterns]
-
-    logger.info(f"Zipping {path} into {zip_file}.")
-
-    with zipfile.ZipFile(zip_file, "a") as zf:
-        existing_files = set(zf.namelist())
-        for root, _, files in os.walk(path):
-            for file in files:
-                if any(pattern.search(file) for pattern in exclude_compiled):
-                    logger.debug(f"Skipping '{file}' in '{path}'.")
-                    continue
-                file_path = os.path.join(root, file)
-                arcname = os.path.relpath(file_path, path)
-                if arcname in existing_files:
-                    logger.debug(f"Skipping duplicate '{arcname}' in '{path}'.")
-                    continue
-                zf.write(file_path, arcname)
-
-
-def zip_experiment(path: str):
-    zip_file = path + ".zip"
-    build_zip(zip_file, path, exclude_patterns=[r"\.pbs\.e\d+", r"\.pbs\.o"])
 
 
 @queue_task(__file__)
